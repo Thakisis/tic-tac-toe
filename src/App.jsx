9b8d0b3b93a97d75dda7dfe01a57514f948720b2
Thakisis <sub722@gmail.com>
2024-10-31 11:10:28 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from '@/ui/button';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Button>Hello</Button>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
